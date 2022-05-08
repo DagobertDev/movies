759869ad83dca0dc02e2d33633cfba4f074b4156
David <git@davidmannes.de>
2022-05-08 19:43:28 +0200

    Add connecting to api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from "@angular/common/http"
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -15,6 +16,7 @@ import { MoviesComponent } from './components/movies/movies.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,25 +1,23 @@
+import { HttpClient } from "@angular/common/http"
 import { Injectable } from '@angular/core';
-import { delay, Observable, of } from "rxjs"
+import { Observable } from "rxjs"
+import { environment } from "../../environments/environment"
 import { Movie } from "../models/movie"
 
-const movies: Movie[] = [
-  {id: 1, name: "Movie 1"},
-  {id: 2, name: "Movie 2"},
-  {id: 3, name: "Movie 3"},
-]
+const url = `${environment.apiUrl}/movies`
 
 @Injectable({
   providedIn: 'root',
 })
 export class MovieService {
-  constructor() {
+  constructor(private httpClient: HttpClient) {
   }
 
   public getAll(): Observable<Movie[]> {
-    return of(movies).pipe(delay(1000))
+    return this.httpClient.get<Movie[]>(url)
   }
 
   public getById(id: number): Observable<Movie | undefined> {
-    return of(movies.find(movie => movie.id == id)).pipe(delay(1000));
+    return this.httpClient.get<Movie>(`${url}/${id}`)
   }
 }
